a12fa974b0d140686859bb9c68909e94bdc8a03d
TychoCinquini <tycho.cinquini@gmail.com>
2023-02-13 19:46:50 -0700

    Fixed custom package importing

--- Homework/homework2.py
+++ Homework/homework2.py
@@ -1,46 +1,10 @@
 # Import required packages
-import numpy as np
 import math
 import matplotlib.pyplot as plt
-
-# Import custom packages
-import mypkg.initialValueProblems as IVP
-
-# Import required python packages
 import numpy as np
 
-# Define class
-class IVP:
-    def __init__(self, a, b, h, w0, f):
-        # Re-assign values
-        self.a = a
-        self.b = b
-        self.h = h
-        self.w0 = w0
-        self.f = f
-
-        # Calculate number of points
-        self.N = int((self.b-self.a)/self.h)
-
-    def explicit_euler(self):
-        # Create solution vectors
-        w = np.zeros(self.N+1)
-        w[0] = self.w0
-        t = np.zeros(self.N+1)
-        t[0] = self.a
-
-        # Iterate across all time steps
-        for i in np.arange(1, self.N+1):
-            # Calculate next time step
-            ti = self.a+(i-1)*self.h
-            t[i] = ti+self.h
-
-            # Calculate next value of solution
-            wi = self.f(ti, w[i-1])
-            w[i] = w[i-1]+self.h*wi
-
-        # Return results
-        return (t, w)
+# Import custom packages
+from mypkg.initialValueProblems import IVP
 
 ## PROBLEM 2
 # Define f(t,y)
