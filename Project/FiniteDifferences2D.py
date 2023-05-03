5e3a7a2433b054f9443739ac2e815e6778bed9e8
TychoCinquini <tycho.cinquini@gmail.com>
2023-05-02 18:12:15 -0600

    Started working on Neumann BC

--- Project/FiniteDifferences2D.py
+++ Project/FiniteDifferences2D.py
@@ -60,8 +60,8 @@ class FiniteDifferences2D:
             self.Aii[(i * (self.n - 1)) - 1, i * (self.n - 1)] = 0
             self.Aii[i * (self.n - 1), (i * (self.n - 1)) - 1] = 0
 
-    # Create Aib submatrix
-    def makeAib(self):
+    # Create Aib submatrix for Dirichlet BC
+    def makeAibDirichlet(self):
         # Initialize matrix
         self.Aib = np.zeros(((self.m - 1) * (self.n - 1), ((self.n + 1) * 2) + (2 * (self.m - 1))))
 
@@ -80,9 +80,59 @@ class FiniteDifferences2D:
             counterX = counterX + (self.n - 1)
             counterY = counterY + 2
 
-    # Create Bib submatrix for Poisson BC
-    def makeBibPoissons(self):
+    # Create Aib submatrix for Neumann BC
+    def makeAibNeumann(self):
+        # Use Dirichlet Aib matrix function to create content of matrix
+        self.makeAibDirichlet()
+        content = self.Aib
+
+        # Initialize matrix
+        self.Aib = np.zeros(((self.m - 1) * (self.n - 1), ((self.n + 1) * 4) + (4 * (self.m - 1)) + 4))
+
+        # Fill in matrix using Aib matrix
+        self.Aib[:, 0:((self.n+1)*2)+(2*(self.m-1))] = content
+
+    # Create Bib submatrix for Dirichlet BC
+    def makeBibDirichlet(self):
         self.Bib = np.eye(((self.n + 1) * 2) + (2 * (self.m - 1)))
+
+    # Create Bib submatrix for Neumann BC
+    def makeBibNeumann(self):
+        # Create empty Bib matrix
+        self.Bib = np.zeros((((self.n+1)*2)+(2*(self.m-1))+4, ((self.n+1)*4)+(4*(self.m-1))+4))
+
+        # Set values
+        self.Bib[0:self.n+1, ((self.n+1)*2)+(2*(self.m-1)):((self.n+1)*2)+(2*(self.m-1))+(self.n+1)] = -1*np.eye(self.n+1)/(2*self.k)
+        for i in np.arange(2*(self.m-1)):
+            self.Bib[self.n+1+i, ((self.n+1)*2)+(2*(self.m-1))+6+i] = math.pow(-1, i+1)/(2*self.h)
+        self.Bib[-5-self.n:-4, -1-self.n:] = np.eye(self.n+1)/(2*self.k)
+        self.Bib[0, self.n+1] = 1/(2*self.k)
+        self.Bib[self.n, (2*self.n)+1] = 1/(2*self.k)
+        self.Bib[self.n+1+(2*(self.m-1)), self.n+1+(2*(self.m-2))] = -1/(2*self.k)
+        self.Bib[1+(2*self.n)+(2*(self.m-1)), (2*self.n+1)+2*(self.m-2)] = -1/(2*self.k)
+        self.Bib[-4, 1] = 1/(2*self.h)
+        self.Bib[-3, self.n-1] = -1/(2*self.h)
+        self.Bib[-4, ((self.n+1)*3)+(2*(self.m-1))] = -1/(2*self.h)
+        self.Bib[-3, ((self.n+1)*3)+(2*(self.m-1))+(self.n-2)] = 1/(2*self.h)
+        self.Bib[-2, self.n+2+(2*(self.m-1))] = 1/(2*self.h)
+        self.Bib[-1, (2*(self.n+1))+(2*(self.m-1))-2] = -1/(2*self.h)
+        self.Bib[-2, -2*self.n] = -1/(2*self.h)
+        self.Bib[-1, -(self.n+2)] = 1/(2*self.h)
+
+        bottom = np.zeros(((2*(self.n+1))+(2*(self.m-1)), ((self.n+1)*4)+(4*(self.m-1))+4))
+        bottom[:, -((2*(self.n+1))+(2*(self.m-1))):] = np.eye((2*(self.n+1))+(2*(self.m-1)))
+        self.Bib = np.concatenate((self.Bib, bottom), axis=0)
+
+    # Create Bii submatrix for Neumann BC
+    def makeBiiNeumann(self):
+        # Create empty matrix
+        self.Bii = np.zeros((((self.n+1)*4)+(4*(self.m-1))+4, (self.m-1)*(self.n-1)))
+
+        # Fill in matrix
+        self.Bii[1:self.n, 0:self.n-1] = np.eye(self.n-1)/(2*self.k)
+        for i in np.arange(2*(self.m-1)):
+            self.Bii[self.n+1+i, i] = math.pow(-1, i)/(2*self.h)
+        self.Bii[(self.n+1)+(2*(self.m-1))+1:(self.n+1)+(2*(self.m-1))+1+(self.n-1), 2*(self.m-2):] = np.eye(self.n-1)/(2*self.k)
 
     # Create f subvector
     def makefvec(self):
@@ -93,8 +143,8 @@ class FiniteDifferences2D:
                 self.fvec[counter] = -1*math.pow(self.h, 2)*self.f(self.x[i], self.y[j])
                 counter = counter + 1
 
-    # Create g subvector
-    def makegvec(self):
+    # Create g subvector for Dirichlet BC
+    def makegvecDirichlet(self):
         self.gvec = np.zeros(((self.n + 1) * 2) + (2 * (self.m - 1)))
         counter = 0
         for i in np.arange(self.n + 1):
@@ -109,16 +159,21 @@ class FiniteDifferences2D:
             self.gvec[counter] = self.g(self.x[i], self.y[-1])
             counter = counter + 1
 
-    # Create linear system with Poisson BC
-    def createLinearSystemPoisson(self):
+    # Create g subvector for Neumann BC
+    def makegvecNeumann(self):
+        self.makegvecDirichlet()
+        self.gvec = np.concatenate((self.gvec, np.array([self.g(self.x[0], self.y[0]), self.g(self.x[self.n], self.y[0]), self.g(self.x[0], self.y[self.m]), self.g(self.x[self.n], self.y[self.m])])))
+
+    # Create linear system with Dirichlet BC
+    def createLinearSystemDirichlet(self):
         # Construct submatrices
         self.makeAii()
-        self.makeAib()
-        self.makeBibPoissons()
+        self.makeAibDirichlet()
+        self.makeBibDirichlet()
 
         # Construct subvectors
         self.makefvec()
-        self.makegvec()
+        self.makegvecDirichlet()
 
         # Create linear system
         self.A = np.zeros(((self.n + 1) * (self.m + 1), (self.n + 1) * (self.m + 1)))
@@ -127,10 +182,51 @@ class FiniteDifferences2D:
         self.A[((self.m - 1) * (self.n - 1)):, ((self.m - 1) * (self.n - 1)):] = self.Bib
         self.bvec = np.concatenate((self.fvec, self.gvec))
 
+    # Create linear system with Neumann BC
+    def createLinearSystemNeumann(self):
+        # Construct submatrices
+        self.makeAii()
+        self.makeAibNeumann()
+        self.makeBiiNeumann()
+        self.makeBibNeumann()
+
+        # Construct subvectors
+        self.makefvec()
+        self.makegvecNeumann()
+
+        print(self.fvec)
+        print(self.gvec)
+
+        import matplotlib.pyplot as plt
+        # print(self.Aii)
+        # plt.spy(self.Aii)
+        # plt.show()
+        # print(self.Aib)
+        # plt.spy(self.Aib)
+        # plt.show()
+        # print(self.Bii)
+        # plt.spy(self.Bii)
+        # plt.show()
+        # print(self.Bib)
+        # plt.spy(self.Bib)
+        # plt.show()
+
+        # Create linear system
+        top = np.concatenate((self.Aii, self.Aib), axis=1)
+        bottom = np.concatenate((self.Bii, self.Bib), axis=1)
+        self.A = np.concatenate((top, bottom), axis=0)
+
+        print(self.A.shape)
+        plt.spy(self.A)
+        plt.show()
+
+        self.bvec = np.concatenate((self.fvec, self.gvec))
+
     # Solve linear system
     def solveLinearSystem(self):
         # Solve linear system
         w = np.linalg.solve(self.A, self.bvec)
+        w = w[:(self.n+1)*(self.m+1)]
 
         # Reorder approximation vector
         self.wl = np.zeros(len(w))
@@ -151,14 +247,21 @@ class FiniteDifferences2D:
         # Return approximation
         return self.wl
 
-    # Solve linear system with Poisson BC
-    def solveLinearSystemPoisson(self):
+    # Solve linear system with Dirichlet BC
+    def solveLinearSystemDirichlet(self):
         # Create and solve linear system using Poisson BC
-        self.createLinearSystemPoisson()
+        self.createLinearSystemDirichlet()
         wl = self.solveLinearSystem()
         return wl
 
-    # Display solution
+    # Solve linear system with Neumann BC
+    def solveLinearSystemNeumann(self):
+        # Create and solve linear system using Neumann BC
+        self.createLinearSystemNeumann()
+        wl = self.solveLinearSystem()
+        return wl
+
+    # Generate variables required for a surface plot
     def surfacePlotElements(self):
         # Create matrices for contour plot
         X, Y = np.meshgrid(self.x, self.y)
--- Project/poissons.py
+++ Project/poissons.py
@@ -7,52 +7,80 @@ import math
 # Import custom packages
 from FiniteDifferences2D import FiniteDifferences2D
 
-# Define meshgrid boundary
-a = 0
-b = 0.5
-c = 0
-d = 0.5
+# # Define meshgrid boundary
+# a = 0
+# b = 0.5
+# c = 0
+# d = 0.5
+#
+# # Define f(x)
+# f = lambda x, y: 0.0
+# f = np.vectorize(f)
+#
+#
+# # Define g(x)
+# def g(x, y):
+#     if x == 0.0:
+#         return 0
+#     elif y == 0.0:
+#         return 0
+#     elif x == 0.5:
+#         return 200*y
+#     elif y == 0.5:
+#         return 200*x
+#
+# # Define u(x,y)
+# u = lambda x, y: 400*x*y
+# u = np.vectorize(u)
+#
+# # Instantiate error vector
+# error = []
+#
+# # Iterate over multiple number of points
+# for num_points in np.array([5, 10, 25, 50, 100]):
+#     # Define number of points along each axis
+#     n = num_points
+#     m = num_points
+#
+#     # Create instance of 2D Finite Differences class
+#     fd1 = FiniteDifferences2D(a, b, c, d, n, m, f, g)
+#
+#     # Solve problem using Poisson BC
+#     _ = fd1.solveLinearSystemDirichlet()
+#
+#     # # Visualize sparsity pattern of matrices
+#     # plt.spy(fd1.Aii)
+#     # plt.show()
+#     # plt.spy(fd1.Aib)
+#     # plt.show()
+#
+#     # Create matrices for contour plot
+#     [X, Y, Wl] = fd1.surfacePlotElements()
+#
+#     # Calculate exact solution
+#     U = u(X, Y)
+#
+#     # Calculate average absolute error
+#     error.append(np.mean(np.abs(U-Wl)))
+#
+# # Plot error
+# plt.plot(np.array([36, 100, 625, 2500, 10000]), error)
+# plt.xlabel("Number of Mesh Points")
+# plt.ylabel("Average Absolute Error")
+# plt.yscale('log')
+# plt.grid(True)
+# plt.show()
+#
+# # Plot contour
+# ax = plt.axes(projection='3d')
+# ax.plot_surface(X, Y, Wl, cmap=cm.viridis)
+# ax.set_xlabel("Width [m]")
+# ax.set_ylabel("Height [m]")
+# ax.set_zlabel("Temperature [°C]")
+# plt.show()
 
-# Define number of points along each axis
-n = 20
-m = 20
 
-
-# Define f(x)
-f = lambda x, y: 0.0
-f = np.vectorize(f)
-
-
-# Define g(x)
-def g(x, y):
-    if x == 0.0:
-        return 0
-    elif y == 0.0:
-        return 0
-    elif x == 0.5:
-        return 200*y
-    elif y == 0.5:
-        return 200*x
-
-
-# Create instance of 2D Finite Differences class
-fd1 = FiniteDifferences2D(a, b, c, d, n, m, f, g)
-
-# Solve problem using Poisson BC
-_ = fd1.solveLinearSystemPoisson()
-
-# Create matrices for contour plot
-[X, Y, Wl] = fd1.surfacePlotElements()
-
-# Plot contour
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Wl, cmap=cm.viridis)
-ax.set_xlabel("Width [m]")
-ax.set_ylabel("Height [m]")
-ax.set_zlabel("Temperature [°C]")
-ax.set_title("Steady-State Heat Distribution in a Thin Square Metal Plate")
-plt.show()
-
+## TEST PROBLEM #2 - NONHOMOGENOUS
 # Define meshgrid boundary
 a = 0
 b = 2
@@ -75,47 +103,72 @@ def g(x, y):
     elif y == 1.0:
         return x * math.exp(1)
 
+# Define g'(x)
+def gp(x, y):
+    if x == 0.0:
+        return math.exp(y)
+    elif y == 0.0:
+        return x * math.exp(y)
+    elif x == 2.0:
+        return math.exp(y)
+    elif y == 1.0:
+        return x * math.exp(y)
+
 
 # Define u(x,y)
 u = lambda x, y: x * math.exp(y)
 u = np.vectorize(u)
 
-# Instantiate error vector
-error = []
+# Instantiate error vectors
+error_poisson = []
 
 # Iterate over multiple number of points
-for num_points in np.array([5, 10, 25, 50, 100]):
+# for num_points in np.array([5, 10, 25, 50, 100]):
+for num_points in np.array([3]):
     # Define number of points along each axis
     n = num_points
     m = num_points
 
-    # Create instance of 2D Finite Differences class
-    fd2 = FiniteDifferences2D(a, b, c, d, n, m, f, g)
+    # Create instances of 2D Finite Differences class
+    fd2_poisson = FiniteDifferences2D(a, b, c, d, n, m, f, g)
+    fd2_neumann = FiniteDifferences2D(a, b, c, d, n, m, f, gp)
 
     # Solve problem using Poisson BC
-    _ = fd2.solveLinearSystemPoisson()
+    _ = fd2_poisson.solveLinearSystemDirichlet()
+
+    # Solve problem using Neumann BC
+    _ = fd2_neumann.solveLinearSystemNeumann()
 
     # Create matrices for contour plot
-    [X, Y, Wl] = fd2.surfacePlotElements()
+    [X_poisson, Y_poisson, Wl_poisson] = fd2_poisson.surfacePlotElements()
+    [X_neumann, Y_neumann, Wl_neumann] = fd2_neumann.surfacePlotElements()
 
     # Calculate exact solution
-    U = u(X, Y)
+    U = u(X_poisson, Y_poisson)
 
     # Calculate average absolute error
-    error.append(np.mean(np.abs(U-Wl)))
+    error_poisson.append(np.mean(np.abs(U-Wl_poisson)))
 
-# Plot error
-plt.plot(np.array([36, 100, 625, 2500, 10000]), error)
-plt.xlabel("Number of Mesh Points")
-plt.ylabel("Average Absolute Error")
-plt.title("Average Absolute Error Versus Number of Mesh Points")
-plt.yscale('log')
-plt.show()
+# # Calculate the order of convergence
+# coeffs = np.polyfit(np.log(np.array([36, 100, 625, 2500, 10000])),np.log(error),1)
+# alpha = coeffs[0]
+# asymptotic_error_constant = np.power(10,coeffs[1])
+# print("The order of convergence is: " + str(alpha))
+# print("The asymptotic error constant is: " + str(asymptotic_error_constant))
+# print("\n")
+
+# # Plot error
+# plt.plot(np.array([36, 100, 625, 2500, 10000]), error)
+# plt.xlabel("Number of Mesh Points")
+# plt.ylabel("Average Absolute Error")
+# plt.yscale('log')
+# plt.xscale('log')
+# plt.grid(True)
+# plt.show()
 
 # Plot contour
 ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Wl, cmap=cm.viridis)
+ax.plot_surface(X_poisson, Y_poisson, Wl_poisson, cmap=cm.viridis)
 ax.set_xlabel("x")
 ax.set_ylabel("y")
-ax.set_title("Approximation of f(x,y)")
 plt.show()
